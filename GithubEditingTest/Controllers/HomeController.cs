3d694bf0ecba60eeb21662eea9fb12902b956dd9
unknown <lsy@lsy-2.klick.com>
2014-12-08 10:56:51 -0500

    Base site commit

---
+++ GithubEditingTest/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GithubEditingTest.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+
+		public ActionResult Article(string id)
+		{
+			int temp;
+			if (Int32.TryParse(id, out temp))
+			{
+				if (1 <= temp && temp <= 3)
+				{
+					return View(String.Format("Article{0}", id));
+				}
+			}
+
+			return Redirect("/");
+		}
+	}
+}
---
+++ GithubEditingTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GithubEditingTest.Startup))]
+namespace GithubEditingTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
